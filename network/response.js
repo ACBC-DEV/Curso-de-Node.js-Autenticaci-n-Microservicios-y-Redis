228fa0e569a3b36d48258241882c0a6095ade572
ACBC <camiloblandon69@gmail.com>
2024-02-18 15:56:52 -0500

    Added success and error response handlers in network/response.js

---
+++ network/response.js
@@ -0,0 +1,19 @@
+exports.success = (req, res, message, status) => {
+  const statusCode = status || 200;
+  const statusMessage = message || "";
+  res.status(statusCode).send({
+    error: false,
+    status,
+    body: message,
+  });
+};
+exports.error = (req, res, message, status) => {
+  const statusCode = status || 500;
+  const statusMessage = message || "Internal Server Error";
+
+  res.status(statusCode).send({
+    error: false,
+    status,
+    body: message,
+  });
+};
