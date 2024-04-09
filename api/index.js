375e4a21db5548595db9db81a32caa9fee1dcd18
acbc <acbc.dev@gmail.com>
2024-04-08 21:42:18 -0500

    Add errors middleware to API index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const auth = require("./components/auth/network");
 const config = require("../config.js");
 const swaggerDoc = require("./swagger.json");
 const options = require("../swagger.js");
+const errors = require("../network/errors");
 
 const app = express();
 
@@ -13,6 +14,8 @@ expressJSDocSwagger(app)(options, swaggerDoc);
 
 app.use("/api/user", user);
 app.use("/api/auth", auth);
+
+app.use(errors);
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.listen(config.api.port, () => {
   console.log("Example app listening on port ", config.api.port);
