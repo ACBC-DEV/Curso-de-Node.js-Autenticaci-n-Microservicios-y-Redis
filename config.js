5f9730318f34576e3138637c89b32a4b1bcd61c2
acbc <acbc.dev@gmail.com>
2024-04-09 20:01:15 -0500

    Update config.js and package.json with MySQL database configuration

--- config.js
+++ config.js
@@ -3,6 +3,12 @@ module.exports = {
     port: process.env.API_PORT || 3000,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || "notasecret!",
+    secret: process.env.JWT_SECRET || "weNeedASecretKey",
+  },
+  mysql: {
+    host: process.env.MYSQL_HOST || "sql10.freemysqlhosting.net",
+    user: process.env.MYSQL_USER || "sql10697950",
+    password: process.env.MYSQL_PASS || "tHn5u3JCzQ",
+    database: process.env.MYSQL_DB || "sql10697950",
   },
 };
