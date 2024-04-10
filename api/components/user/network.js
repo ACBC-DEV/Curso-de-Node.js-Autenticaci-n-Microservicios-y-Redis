c3ef286d387c7625277095b5da1c3c7e663c575e
acbc <acbc.dev@gmail.com>
2024-04-09 20:01:34 -0500

    Refactor user component to use MySQL database instead of dummy data

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -12,52 +12,46 @@ router.get("/:id", getByID);
 router.post("/", postUser);
 router.delete("/:id", deleteUser);
 router.put("/", secure("update"), putUser);
-function getUser(req, res) {
+function getUser(req, res, next) {
   Controller.list()
     .then((lista) => {
       response.success(req, res, lista, 200);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 }
 
-function getByID(req, res) {
+function getByID(req, res, next) {
   Controller.get(req.params.id)
     .then((user) => {
       response.success(req, res, user, 200);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 }
 
-async function postUser(req, res) {
+async function postUser(req, res, next) {
   try {
     const list = await Controller.upsert(req.body);
     console.log(req.body);
     response.success(req, res, list, 201);
   } catch (err) {
-    response.error(req, res, err.message, 500);
+    next(err);
   }
 }
 
-async function deleteUser(req, res) {
+async function deleteUser(req, res, next) {
   try {
     const list = await Controller.remove(req.params.id);
     console.log(req.body);
     response.success(req, res, list, 201);
   } catch (err) {
-    response.error(req, res, err.message, 500);
+    next(err);
   }
 }
-function putUser(req, res) {
+function putUser(req, res, next) {
   Controller.update(req.body)
     .then((data) => {
       response.success(req, res, data, 200);
     })
-    .catch((err) => {
-      response.error(req, res, err.message, 500);
-    });
+    .catch(next);
 }
 module.exports = router;
