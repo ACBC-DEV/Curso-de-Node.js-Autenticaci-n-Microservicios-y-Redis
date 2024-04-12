7df7234851cc7d7e2936ab230b40a42f7ec05bdd
acbc <acbc.dev@gmail.com>
2024-04-11 22:50:54 -0500

    Refactor authentication middleware, update login and user components, and fix owner check in auth/index.js

--- api/components/user/secure.js
+++ api/components/user/secure.js
@@ -1,11 +1,18 @@
 const auth = require("../../../auth");
+const stataments = {
+  UPDATE: "update",
+  FOLLOW: "follow",
+};
 module.exports = function checkAuht(action) {
   function middleware(req, res, next) {
     switch (action) {
-      case "update":
+      case stataments.UPDATE:
         const owner = req.body.id;
         auth.check.own(req, owner);
-
+        next();
+        break;
+      case stataments.FOLLOW:
+        auth.check.logged(req);
         next();
         break;
       default:
